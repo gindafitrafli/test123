62fd8714ac9e44a3ade6fed25c41a41cdea3ddff
gfakbar20 <gfakbar20@gmail.com>
2026-06-04 12:48:56 +0700

    kafka config

--- employeefacing/src/main/java/com/project/employeefacing/config/KafkaProducerConfig.java
+++ employeefacing/src/main/java/com/project/employeefacing/config/KafkaProducerConfig.java
@@ -7,14 +7,17 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
-public class KafkaConfig {
+@EnableKafka
+public class KafkaProducerConfig {
 
-    @Value("${producer.id: }")
+    @Value("${broker.uri:localhost}")
+    String brokerUri;
 
     @Bean
     public ProducerFactory producerFactory() {
@@ -24,9 +27,10 @@ public class KafkaConfig {
     @Bean
     public Map producerConfigs() {
         Map props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, );
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUri);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
 
         return props;
     }
